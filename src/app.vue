64eb03f224a95760f132f58b20f881b19d855bd2
Ivan Timofeev <ivan.timofeev@bsc-ideas.com>
2019-02-17 05:36:20 +0300

    #SU4NA-3: Сверстать макет на vue.js

--- src/app.vue
+++ src/app.vue
@@ -11,7 +11,7 @@
         </div>
 
         <chpa></chpa>
-        <news></news>
+        <!--news></news-->
 
     </div>
 
--- src/components/chpa.vue
+++ src/components/chpa.vue
@@ -1,11 +1,22 @@
 <template>
     <div>
         <div class="chpa-component">
-            {{name}}
-            <br/>
-            <a v-bind:href="url">Перейти на shikimori.org</a>
-            <br/>
-            <img v-bind:src="poster_url" alt="image" />
+            <div class="chpa_phrase">
+                <div>Есть в названии:</div>
+                <div><input type="text" v-model="phrase" /></div>
+                <div>
+                    <button v-on:click="search">Найти</button>
+                </div>
+            </div>
+            <div class="chpa_animelist">
+                <div v-for="a in animes">
+                    {{a.name}}
+                    <br/>
+                    <a v-bind:href="a.url">Перейти на shikimori.org</a>
+                    <br/>
+                    <img v-bind:src="a.poster_url" alt="image" />
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -24,6 +35,43 @@
         left: 950px;
         top: 100px;
     }
+    .chpa_phrase {
+        position: absolute;
+        left: 50px;
+        top: 50px;
+    }
+    .chpa_phrase div {
+        display: inline-block;
+        margin-left: 40px;
+    }
+    .chpa_phrase div input {
+        height: 30px;
+        width: 200px;
+        border-radius: 15px;
+        border-style: solid;
+        border-width: 5px;
+        border-color: #BEF6B9;
+        text-indent: 10px;
+    }
+    .chpa_phrase div button {
+        position: absolute;
+        top: 0px;
+        height: 40px;
+        width: 200px;
+        border-radius: 15px;
+        border-style: solid;
+        border-width: 5px;
+        background-color: green;
+        border-color: green;
+        color: white;
+        font-size: 20px;
+    }
+    .chpa_animelist {
+        position: absolute;
+        top: 100px;
+        left: 10px;
+    }
+
 </style>
 
 <script>
@@ -34,19 +82,25 @@
         name: 'chpa',
         data() {
             return {
-                name: 'chpa-component',
-                url: 'chpa-component',
-                poster_url: 'chpa-component'
+                animes: [],
+                phrase: ""
             };
         },
+        methods: {
+            search: function (event) {
+                axios
+                    .get('http://localhost:10045/animes/search', {
+                        params: {
+                            phrase: this.phrase
+                        }
+                    })
+                    .then(response => {
+                        this.animes = response.data;
+                    });
+            }
+        },
         mounted () {
-            axios
-                .get('http://localhost:10045/animes/random')
-                .then(response => {
-                    this.name = response.data.name;
-                    this.url = response.data.url;
-                    this.poster_url = response.data.poster_url;
-                });
+
         }
     }
 </script>
