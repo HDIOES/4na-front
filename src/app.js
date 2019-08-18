35c898cd338159723d5d11a716a42dc6d1e9b5ce
ShadowHD33RUS <shadowhd33rus@gmail.com>
2019-08-19 00:24:07 +0300

    Remove unnecessary objects

--- src/app.js
+++ src/app.js
@@ -1,24 +1,11 @@
 import Vue from 'vue'
 import App from './app.vue'
-import Chpa from './components/chpa.vue'
-import News from './components/news.vue'
 import './app.css'
 import './css/other.css'
 
 new Vue({
+    //router,
+    //vuetify,
     el: '#app',
     render: h => h(App)
-
-});
-
-var chpa = new Vue({
-    el: '#chpa',
-    render: h => h(Chpa)
-
-});
-
-var news = new Vue({
-    el: '#news',
-    render: h => h(News)
-
 });
